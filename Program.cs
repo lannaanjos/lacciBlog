740a70c68c6bec053a7a1d92ca2ec565f9da4071
Lanna <lannadosanjoscosta@gmail.com>
2025-11-09 18:07:30 -0300

    repo init

---
+++ Program.cs
@@ -0,0 +1,36 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/usuarios", () =>
+{
+    List<String> nomes = new List<String>
+    {
+        "Sophie",
+        "Bela",
+        "Smi"
+    };
+
+    return nomes;
+});
+
+app.MapPost("/usuarios", (string nome) =>
+{
+    return Results.Ok($"Usuário {nome} adicionado com sucesso!");
+});
+
+app.MapPut("/usuarios", (int id, string novoNome) =>
+{
+    return Results.Ok($"usuário {novoNome} atualizado com sucesso!");
+});
+
+app.MapPatch("/usuarios", (int id, string campo, string valor) =>
+{
+    return Results.Ok($"Usuário {id} teve o campo {campo} atualizado para {valor}");
+});
+
+app.MapDelete("/usuarios", (int id) =>
+{
+    return Results.Ok($"Usuário {id} deletado com sucesso!");
+});
+
+app.Run();
